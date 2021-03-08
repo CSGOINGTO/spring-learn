5ece67fc9d37572c8c3170b29b8b34cdd5c4d869
CSGOINGTO <liuxuenbrw@163.com>
2021-03-08 16:12:05 +0800

    注解配置类

--- my-spring/src/main/java/com/lx/config/FirstConfig.java
+++ my-spring/src/main/java/com/lx/config/FirstConfig.java
@@ -2,8 +2,10 @@ package com.lx.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
+//@EnableAspectJAutoProxy(proxyTargetClass = true) // 开启@AspectJ注解配置,并强制使用Cglib
 @ComponentScan(basePackages = "com.lx")
 public class FirstConfig {
 
